91bcbb3e07887a596d89f2aa61741c4566bbb528
WoobinHwang <dnqls0326@naver.com>
2023-06-05 05:01:12 +0900

    restart : base of project

---
+++ SR/src/main/java/com/mysite/sr/MainController.java
@@ -0,0 +1,17 @@
+package com.mysite.sr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+	@GetMapping("main")
+	public String MainPage() {
+		return "MainPage";
+	}
+}
