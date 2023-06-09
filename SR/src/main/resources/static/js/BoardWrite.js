617752357ff05857e7285b0b1430df95a2473033
WoobinHwang <dnqls0326@naver.com>
2023-06-09 20:52:41 +0900

    if you input image file, page is append li tag on BoardWritePage.

--- SR/src/main/resources/static/js/BoardWrite.js
+++ SR/src/main/resources/static/js/BoardWrite.js
@@ -1,42 +1,60 @@
 var uploadFiles = [];
 
+const imgInput = document.querySelector('.imgInput');
+const imgList = document.querySelector('.imgList');
+const target = document.querySelector('.target');
+
+imgInput.addEventListener('change', getImageFiles);
+
+// 아래 두개의 함수는 같은 역할
+// 1.
+// upload.addEventListener('click', () => divUpload.click());
+
+// 2.
+// upload.addEventListener('click', function() {
+// 	divUpload.click()
+// })
+
 function getImageFiles(e){
 	const file = e.currentTarget.files[0];
-	const imagePreview = document.querySelector('.image-preview');
+	const imagePreview = document.querySelector('.preview');
 	// const docFrag = new DocumentFragment();
 	
 	console.log(typeof file, file);
+	console.log(typeof file.name);
+	console.log(file.name);
 	console.log(e);
-	
+	console.log(uploadFiles);
 	
 	// 리스트 uploadFiles 에 파일을 추가
     uploadFiles.push(file);
     const reader = new FileReader();
     reader.onload = (e) => {
-    	const preview = createElement(e, file);
-    	imagePreview.appendChild(preview);
+    	imagePreview.setAttribute('src', e.target.result)
+    	imagePreview.style.display = "";
+    	
+    	const liTag = createLiTag(e, file);
+    	imgList.appendChild(liTag);
 	};
+	
+	// imagePreview.setAttribute('src', e.target.result)
+	// imagePreview.style.display = "";
+	// const liTag = createLiTag(e);
+	// imgList.appendChild(liTag);
+	
     reader.readAsDataURL(file);
-    
-    
 }
 
-function createElement(e, file) {
-      const li = document.createElement('li');
-      const img = document.createElement('img');
-      img.setAttribute('src', e.target.result);
-      img.setAttribute('data-file', file.name);
-      li.appendChild(img);
-
-      return li;
-    }
-
-function test(){
-	console.log("testing...");
+function  createLiTag(e, file) {
+	const li = document.createElement('li');
+	const imgName = file.name;
+	// const textNode = document.createTextNode(file.name);
+	const textNode = document.createTextNode(imgName);
+	li.appendChild(textNode);
+	
+	console.log("////////////");
+	console.log(file);
+	console.log(file.name);
+	
+	return li;
 }
-
-const realUpload = document.querySelector('.real-upload');
-const upload = document.querySelector('.upload');
-
-upload.addEventListener('click', () => realUpload.click());
-realUpload.addEventListener('change', getImageFiles);
