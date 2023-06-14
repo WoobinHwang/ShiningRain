4a9f2d00048a4b169cdc04fbc942dbaf37d4c7ab
WoobinHwang <dnqls0326@naver.com>
2023-06-14 16:05:34 +0900

    if click 'X' button, remove list element and document element

--- SR/src/main/resources/static/js/BoardWrite.js
+++ SR/src/main/resources/static/js/BoardWrite.js
@@ -3,6 +3,7 @@ var uploadFiles = [];
 const imgInput = document.querySelector('.imgInput');
 const imgList = document.querySelector('.imgList');
 const target = document.querySelector('.target');
+const imgButton = document.querySelector('.imgList li input');
 
 imgInput.addEventListener('change', getImageFiles);
 
@@ -18,43 +19,81 @@ imgInput.addEventListener('change', getImageFiles);
 function getImageFiles(e){
 	const file = e.currentTarget.files[0];
 	const imagePreview = document.querySelector('.preview');
-	// const docFrag = new DocumentFragment();
 	
-	console.log(typeof file, file);
-	console.log(typeof file.name);
-	console.log(file.name);
-	console.log(e);
-	console.log(uploadFiles);
+	// console.log(typeof file, file);
+	// console.log(typeof file.name);
+	// console.log(file.name);
+	// console.log(e);
+	// console.log(uploadFiles);
 	
 	// 리스트 uploadFiles 에 파일을 추가
+	for (i = 0; i < uploadFiles.length; i++){
+		if(uploadFiles[i].name == file.name){
+			alert("이미 올린 이미지입니다.");
+			return;
+		}
+	}
+	
+	
     uploadFiles.push(file);
     const reader = new FileReader();
     reader.onload = (e) => {
     	imagePreview.setAttribute('src', e.target.result)
     	imagePreview.style.display = "";
     	
-    	const liTag = createLiTag(e, file);
+    	const liTag = createLiTag(file);
     	imgList.appendChild(liTag);
 	};
-	
-	// imagePreview.setAttribute('src', e.target.result)
-	// imagePreview.style.display = "";
-	// const liTag = createLiTag(e);
-	// imgList.appendChild(liTag);
 	
     reader.readAsDataURL(file);
 }
 
-function  createLiTag(e, file) {
+function createLiTag(file) {
 	const li = document.createElement('li');
 	const imgName = file.name;
-	// const textNode = document.createTextNode(file.name);
 	const textNode = document.createTextNode(imgName);
-	li.appendChild(textNode);
+	const inputButton = document.createElement('input');
 	
-	console.log("////////////");
-	console.log(file);
-	console.log(file.name);
+	li.appendChild(textNode);
+	// span.appendChild(textNode);
+	// button.appendChild(xNode);
+	inputButton.setAttribute('type', 'button')
+	inputButton.setAttribute('value', 'X')
+	inputButton.setAttribute('onclick', 'removeImage("' +imgName+ '")');
+	li.appendChild(inputButton);
+	
+	// console.log("////////////");
+	// console.log(file);
+	// console.log(file.name);
 	
 	return li;
+}
+
+/** name과 같은 요소를 리스트와 li태그에서 제거 */
+function removeImage(name){
+	
+	// uploadFiles 리스트에서 name과 같은 요소 제거
+	for (i = 0; i < uploadFiles.length; i++){
+		if(uploadFiles[i].name == name){
+			uploadFiles.splice(i, 1);
+			alert(i + "번째 리스트 삭제 완료")
+			// console.log(i + "번째 리스트 삭제 완료")
+			// console.log(uploadFiles);
+		}
+		
+	}
+	
+	// X 버튼 클릭한 element를 지우기
+	$(".imgList li").each(function() {
+		console.log($(this).text());
+		console.log(this);
+		
+		if($(this).text() == name){
+			console.log("발견");
+			imgList.removeChild(this);
+		}
+	});
+	
+	console.log("남은 files ->")
+	console.log(uploadFiles)
 }
